9f714b168b48136ea4f605103c5bffc90541ece5
raven <trannhathoang8678@gmail.com>
2021-08-06 03:17:17 +0700

    add structure

---
+++ src/test/java/fcodelol/clone/juno/JunoApplicationTests.java
@@ -0,0 +1,13 @@
+package fcodelol.clone.juno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
