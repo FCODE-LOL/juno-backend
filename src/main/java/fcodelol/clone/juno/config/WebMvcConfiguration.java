41c1b37ae388e8ec343e403f766f2de9e98c2bb3
raven <trannhathoang8678@gmail.com>
2021-08-10 12:43:39 +0700

    add web config

--- src/main/java/fcodelol/clone/juno/config/WebMvcConfiguration.java
+++ src/main/java/fcodelol/clone/juno/config/WebMvcConfiguration.java
@@ -1,9 +1,24 @@
 package fcodelol.clone.juno.config;
 
+import fcodelol.clone.juno.interceptor.GatewayInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Autowired
+    GatewayInterceptor gatewayInterceptor;
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(gatewayInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET","PUT","POST","DELETE");
+    }
 
 }
